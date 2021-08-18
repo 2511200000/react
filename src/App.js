afb20c721bd5bc889700be5a1e40af83ba5fdd86
2511200000 <khanhkhanh251100@gmail.com>
2021-08-18 21:18:29 +0700

    new edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Slider from './components/Slider';
-import HomePage from './components/HomePage';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import HomePage from "./components/HomePage";
 function App() {
   return (
-   <div>
-     <Header/>
-     <Slider/>
-     <HomePage/>
-      <Footer/>
+    <div>
+      <Header />
+      <Slider />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
