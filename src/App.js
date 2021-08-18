1d3bb6a363cda0af38566d1170c6d9403feaa42a
2511200000 <khanhkhanh251100@gmail.com>
2021-08-18 20:44:52 +0700

    compelete some components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Slider from './components/Slider';
+import HomePage from './components/HomePage';
+function App() {
+  return (
+   <div>
+     <Header/>
+     <Slider/>
+     <HomePage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
